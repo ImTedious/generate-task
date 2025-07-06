d1ac956dc3a03838604cbc71474d6af52dc6f2b6
Danial <admin@redsparr0w.com>
2025-07-07 11:20:59 +1200

    Update LogMasterPlugin.java

--- src/main/java/com/logmaster/LogMasterPlugin.java
+++ src/main/java/com/logmaster/LogMasterPlugin.java
@@ -54,6 +54,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.stream.Collectors;
 
