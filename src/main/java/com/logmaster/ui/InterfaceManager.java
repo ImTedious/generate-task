1201e91f2a2c26ff8b53d8be9e19a60d3f1e7853
RedSparr0w <RedSparr0w@users.noreply.github.com>
2025-07-08 21:38:21 +1200

    Fixup flickering between tasklist and dashboard

--- src/main/java/com/logmaster/ui/InterfaceManager.java
+++ src/main/java/com/logmaster/ui/InterfaceManager.java
@@ -308,6 +308,7 @@ public class InterfaceManager implements MouseListener, MouseWheelListener {
                     this.taskDashboard.setVisibility(false);
                     this.taskList.refreshTasks(0);
                     this.taskList.setVisibility(true);
+                    showTabs();
                 });
                 tabIndex++;
             }
@@ -404,8 +405,13 @@ public class InterfaceManager implements MouseListener, MouseWheelListener {
     }
 
     public void completeTask() {
+        boolean wasDashboardVisible = this.taskDashboard.isVisible();
         this.taskDashboard.updatePercentages();
         taskList.refreshTasks(0);
+        // Restore previous visibility state
+        this.taskDashboard.setVisibility(wasDashboardVisible);
+        this.taskList.setVisibility(!wasDashboardVisible);
+        showTabs();
     }
 
     public void clearCurrentTask() {
